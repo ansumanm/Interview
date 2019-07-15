b88ccbb5eef292c6edc65268b9941f8fb5c4f904
Ansuman Mohanty <ansuman.mohanty@gmail.com>
2019-07-16 01:06:33 +0530

    Factory pattern, private Destructor, prototype pattern

---
+++ cplusplus/src/privateDesEx.cpp
@@ -0,0 +1,29 @@
+// CPP program to illustrate 
+// Private Destructor 
+#include <iostream> 
+
+// A class with private destuctor 
+class Test { 
+private: 
+    ~Test() {} 
+    friend void destructTest(Test*); 
+}; 
+
+// Only this function can destruct objects of Test 
+void destructTest(Test* ptr) 
+{ 
+    std::cout << "Calling private destructor" << std::endl;
+    delete ptr; 
+} 
+
+int main() 
+{ 
+    // create an object 
+    Test* ptr = new Test; 
+
+    // destruct the object 
+    destructTest(ptr); 
+
+    return 0; 
+} 
+
