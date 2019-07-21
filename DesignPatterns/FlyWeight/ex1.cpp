27323e808e5a908e11d9f1850f982c5516ed150c
Ansuman Mohanty <ansuman.mohanty@gmail.com>
2019-07-21 22:07:31 +0530

    Flyweight pattern with bimap.

--- DesignPatterns/FlyWeight/ex1.cpp
+++ DesignPatterns/FlyWeight/ex1.cpp
@@ -4,22 +4,118 @@
  */
 
 #include<iostream>
-#include<boost/bimap.h>
+#include<map>
 
 using namespace std;
 
 typedef unsigned short key;
 
-class User {
-    protected:
-        static bimap<key, string> names;
-        static key seed; // Initial value
+template <typename X, typename Y>
+class mybimap {
+	public:
+		map<X, Y> X_to_Y;
+		map<Y, X> Y_to_X;
+
+		// Find X from Y
+		std::pair<X, bool> find(const Y& y) {
+			typename map<Y, X>::iterator it = Y_to_X.find(y);
+
+			if (it == Y_to_X.end()) {
+				return std::make_pair(X(), false);
+			} else {
+				return std::make_pair(it->second, true);
+			}
+		}
+
+		void add(const std::pair<X, Y>& X_Y_pair) {
+			X_to_Y.insert(X_Y_pair);
+			Y_to_X.insert(std::make_pair(X_Y_pair.second, X_Y_pair.first));
+		}
+
+		// Find Y from X
+		std::pair<Y, bool> find(const X& x) {
+			typename map<X, Y>::iterator it = X_to_Y.find(x);
+
+			if (it == X_to_Y.end()) {
+				return std::make_pair(Y(), false);
+			} else {
+				return std::make_pair(it->second, true);
+			}
+		}
 };
 
-key User::seed = 1;
-bimap<key, string> User::names{};
+class User {
+	protected:
+		key first_name;
+		key last_name;
+
+		static mybimap<key, string> names;
+		static key seed; // Initial value
+
+		static key add(const string &s) {
+			/* Check if the string exists */
+			std::pair<key, bool> key_bool_pair;
+
+			key_bool_pair = names.find(s);
+
+			if (key_bool_pair.second == true) {
+				// The string exists. return key.
+				return key_bool_pair.first;
+			} else {
+				// Create a key and string pair.
+				seed++;
+				names.add(std::make_pair(seed, s));
+				return seed;
+			}
+		}
+
+		const string get_name_from_key(key k) {
+			std::pair<string, bool> str_bool_pair;
+
+			str_bool_pair = names.find(k);
+
+			if (str_bool_pair.second == true) {
+				return str_bool_pair.first;
+			} else {
+				return string();
+			}
+		}
+
+	public:
+		// Constructor
+		User(const string& fn, const string& ln):
+			first_name(add(fn)), last_name(add(ln)) {}
+
+		const string get_first_name() {
+			return get_name_from_key(first_name);
+		}
+
+		const string get_last_name() {
+			return get_name_from_key(last_name);
+		}
+
+		void print_keys() {
+			cout << first_name << " " << last_name << endl;
+		}
+};
+
+key User::seed = 0;
+mybimap<key, string> User::names{};
 
 int main() 
 {
+    User u1("Alia", "Bhat");
+    User u2("Mahesh", "Bhat");
+    User u3("Bhat", "Vikram");
+
+    cout << u1.get_first_name() << " " << u1.get_last_name() << endl;
+    u1.print_keys();
+
+    cout << u2.get_first_name() << " " << u2.get_last_name() << endl;
+    u2.print_keys();
+
+    cout << u3.get_first_name() << " " << u3.get_last_name() << endl;
+    u3.print_keys();
+
     return 0;
 }
