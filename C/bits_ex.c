9ca2c9db731bb7be89adb5bf96f423048fa66147
Ansuman Mohanty <ansuman.mohanty@gmail.com>
2019-08-19 00:54:40 +0530

    bits range set/clear

---
+++ C/bits_ex.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdint.h>
+
+uint32_t get_mask(int start, int end)
+{
+	uint32_t mask = -1;
+	uint32_t mask_right;
+	uint32_t mask_left;
+
+	mask_right = (-1 << start);
+	mask_left = (mask >> (32 - (end + 1)));
+	mask = mask_right & mask_left;
+
+	return mask;
+}
+void clear_bits(uint32_t *i, int start, int end)
+{
+	uint32_t mask = get_mask(start, end);
+	*i = *i & ~mask;
+}
+
+void set_bits(uint32_t *i, int start, int end)
+{
+	uint32_t mask = get_mask(start, end);
+	*i = *i | mask;
+}
+
+int main()
+{
+	uint32_t i = -1;
+	printf("0x%x \n", i);
+
+	clear_bits(&i, 4, 7);
+	printf("0x%x \n", i);
+
+	clear_bits(&i, 12, 15);
+	printf("0x%x \n", i);
+
+	set_bits(&i, 4, 7);
+	printf("0x%x \n", i);
+
+	return 0;
+}
